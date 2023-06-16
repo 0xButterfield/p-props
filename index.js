4e2e41023ee5bda345ed070e3a17b7cafcefcece
0xbutterfield <brianbutterfield.emailaddress@gmail.com>
2023-06-16 11:24:13 +0200

    fix: mapper

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import pMap from 'p-map'
 export default async function pProps(input, options = undefined) {
   const isMap = input instanceof Map
   const entries = isMap ? [...input.entries()] : Object.entries(input)
-  const values = await pMap(entries, options)
+  const values = await pMap(entries, async ([, value]) => await value, options)
   const mappedEntries = entries.map(([key], index) => [key, values[index]])
   return isMap ? new Map(mappedEntries) : Object.fromEntries(mappedEntries)
 }
